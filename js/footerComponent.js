15fe093710fd556e335c9f82c774debdb5acbad5
Ushakov7352 <vlad1952@yandex.ru>
2019-06-20 20:09:26 +0300

    добавил footer component

---
+++ js/footerComponent.js
@@ -0,0 +1,83 @@
+Vue.component('footer-shop',{
+    template:`<div>
+<div class="container content4">
+        <figure class="img1">
+            <img src="img/Layer_40.png" alt="">
+        </figure>
+           <div class="text">
+            <p>“Vestibulum quis porttitor dui! Quisque viverra nunc mi,
+                a pulvinar purus condimentum a. Aliquam condimentum mattis neque sed pretium”</p>
+            <h5 class="txt1">Bin Burhan</h5>
+            <h6 class="txt2">Dhaka, Bd</h6>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+        </div>
+
+        <div class="text2">
+            <h4>Subscribe</h4>
+                <p>FOR OUR NEWLETTER AND PROMOTION</p>
+        </div>
+        <div class="form1">
+            <form action="#">
+                <input type="email" placeholder="Enter Your Email">
+                <input type="submit" value="Subscribe">
+            </form>
+        </div>
+    </div>
+    <div class="container clr"></div>
+    <div class="footer">
+        <div class="footer1">
+            <a href="index.html" class="logo"><img class="logo__img" src="img/logo.png" alt="logo">BRAN<span class="logo__d">D</span></a>
+            <p>Objectively transition extensive data rather than cross functional solutions. Monotonectally syndicate multidisciplinary materials before go forward benefits. Intrinsicly syndicate an expanded array of processes and cross-unit partnerships.
+            <br><br><br>
+            Efficiently plagiarize 24/365 action items and focused infomediaries.
+            Distinctively seize superior initiatives for wireless technologies. Dynamically optimize..</p></div>
+        <nav class="footer2">
+            <h3>COMPANY</h3>
+            <ul>
+                <li><a href="index.html">Home </a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">How It Works</a></li>
+                <li><a href="#">Contact</a></li>
+        </ul>
+        </nav>
+        <nav class="footer3">
+            <h3>INFORMATION</h3>
+            <ul>
+                <li><a href="#">Tearms &amp; Condition </a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">How to Buy</a></li>
+                <li><a href="#">How to Sell</a></li>
+                <li><a href="#">Promotion</a></li>
+        </ul>
+        </nav>
+        <nav class="footer4">
+            <h3>SHOP CATEGORY</h3>
+            <ul>
+                <li><a href="#">Men</a></li>
+                <li><a href="#">Women</a></li>
+                <li><a href="#">Child</a></li>
+                <li><a href="#">Apparel</a></li>
+                <li><a href="#">Brows All</a></li>
+        </ul>
+        </nav>
+    </div>
+    <div class="endline">
+       <div class="endline-content">
+        <div class="endline-content1">
+            <p>&copy; 2017  Brand  All Rights Reserved.</p>
+        </div>
+        <div class="endline-content2">
+            <a href="#"><img src="img/fb.png" alt=""></a>
+            <a href="#"><img src="img/twitter3.png" alt=""></a>
+            <a href="#"><img src="img/linkedin.png" alt=""></a>
+            <a href="#"><img src="img/pinterest.png" alt=""></a>
+            <a href="#"><img src="img/google.png" alt=""></a>
+        </div>
+    </div>
+    </div>
+
+</div>`
+});
